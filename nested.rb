8a3185c0ca0f806a2b21326361ca5bec14b384fb
Nataliya R <n.rudenko@gmail.com>
2019-02-03 06:24:27 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -103,7 +103,8 @@ def changing_alan
           :languages => ["C"]
         }
      }
-    programmer_hash = [:alan_kay][:known_for] ="GUI"
+     alans_new_info = "GUI"
+    programmer_hash[:alan_kay][:known_for] = alans_new_info
     puts programmer_hash
      
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
